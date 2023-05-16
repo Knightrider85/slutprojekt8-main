93e89c4a0113127f9245214a712a63ad049c9166
Knightrider85 <daniel@grevensgastro.se>
2023-05-16 14:32:18 +0200

    mongodb server startup

--- server/server.ts
+++ server/server.ts
@@ -1,12 +1,20 @@
 import express from "express";
+import mongoose from "mongoose";
 
 const app = express();
-const url = "mongodb://localhost:27017";
+const url = 'mongodb+srv://Knightrider:<Slutproject8>@cluster0.lv2qtau.mongodb.net/stepup?retryWrites=true&w=majority'; // Your MongoDB Atlas connection URI
 const port = 3000;
 
+// Connect to the MongoDB Atlas cluster using Mongoose
+mongoose.connect(url)
+  .then(() => {
+    console.log("Connected to MongoDB Atlas");
 
-
-
-app.listen(port, () => {
-    console.log()
-})
+    // Start the server after connecting to the database
+    app.listen(port, () => {
+      console.log(`Server started on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Failed to connect to MongoDB Atlas:", err);
+  });
