af3e2ce89ee914853e60baac34d5adea801dde10
JennyWeij <jennyweij@outlook.com>
2023-05-19 14:19:13 +0200

    ändrat formcontrol till text

--- client/src/components/LoginForm.tsx
+++ client/src/components/LoginForm.tsx
@@ -8,7 +8,7 @@ function LoginForm() {
       <Form>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Username</Form.Label>
-          <Form.Control type="email" placeholder="Enter username" />
+          <Form.Control type="text" placeholder="Enter username" />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
