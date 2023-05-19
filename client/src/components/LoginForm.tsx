4fb870fa738cf13182ef19161d0486008cd959e2
JennyWeij <jennyweij@outlook.com>
2023-05-19 14:35:59 +0200

    börjat lägga till Yup

--- client/src/components/LoginForm.tsx
+++ client/src/components/LoginForm.tsx
@@ -1,24 +1,71 @@
+import { ErrorMessage, Field, Formik } from "formik";
+import React from "react";
 import { Container } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import { useNavigate } from "react-router-dom";
+import * as Yup from "yup";
+
+interface FormValues {
+  username: string;
+  password: string;
+}
+
+const validationSchema = Yup.object().shape({
+  username: Yup.string().required("Username is required"),
+  password: Yup.string()
+    .min(6, "Password must be at least 6 characters")
+    .required("Password is required"),
+});
 
 function LoginForm() {
+  const navigate = useNavigate();
+  const [loginError, setLoginError] = React.useState<string>(" ");
+
+  const handleSubmit = async (values: FormValues) => {
+    console.log("Form data:", values);
+    const success = await (values.username, values.password);
+    if (success) {
+      navigate("/");
+    } else {
+      setLoginError("Something went wrong. Please try again.");
+    }
+  };
+
   return (
     <Container className="d-flex justify-content-center align-items-center vh-60">
-      <Form>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Username</Form.Label>
-          <Form.Control type="text" placeholder="Enter username" />
-        </Form.Group>
+      <Formik
+        initialValues={{ username: "", password: "" }}
+        validationSchema={validationSchema}
+        onSubmit={handleSubmit}
+      >
+        {({ handleSubmit }) => (
+          <Form onSubmit={handleSubmit}>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Username</Form.Label>
+              <Field type="text" name="username" className="form-control" />
+              <ErrorMessage
+                name="username"
+                component="div"
+                className="text-danger"
+              />
+            </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Enter password" />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Login
-        </Button>
-      </Form>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Field type="password" name="password" className="form-control" />
+              <ErrorMessage
+                name="password"
+                component="div"
+                className="text-danger"
+              />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Login
+            </Button>
+          </Form>
+        )}
+      </Formik>
     </Container>
   );
 }
