fa06258308f25020de14f14a1bd4a3ad121ffbdc
JennyWeij <jennyweij@outlook.com>
2023-05-19 11:18:21 +0200

    loginform på plats och centrerat

--- client/src/components/LoginForm.tsx
+++ client/src/components/LoginForm.tsx
@@ -1,22 +1,25 @@
+import { Container } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 
 function LoginForm() {
   return (
-    <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Username</Form.Label>
-        <Form.Control type="email" placeholder="Enter username" />
-      </Form.Group>
+    <Container className="d-flex justify-content-center align-items-center vh-60">
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Username</Form.Label>
+          <Form.Control type="email" placeholder="Enter username" />
+        </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Enter password" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Login
-      </Button>
-    </Form>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Enter password" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Login
+        </Button>
+      </Form>
+    </Container>
   );
 }
 
