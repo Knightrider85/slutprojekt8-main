26a5751ff4b039948468b802617c0eebd839bb36
Martina Kreivi <martinakreivi@gmail.com>
2023-05-19 13:20:59 +0200

    test user

--- client/src/components/CreateUser.tsx
+++ client/src/components/CreateUser.tsx
@@ -2,7 +2,6 @@ import { Formik } from "formik";
 import Button from "react-bootstrap/Button";
 import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
-import InputGroup from "react-bootstrap/InputGroup";
 import Row from "react-bootstrap/Row";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
@@ -11,26 +10,15 @@ import { useOrderContext } from "../contexts/OrderContext";
 
 const schema = Yup.object().shape({
   name: Yup.string().required(),
-  address: Yup.string().required(),
-  city: Yup.string().required(),
-  zip: Yup.string()
-    .required()
-    .matches(/^[0-9]{5}$/, "Zipcode number must be exactly 5 digits"),
+
   email: Yup.string().email("Invalid email address").required(),
-  phone: Yup.string()
-    .required()
-    .matches(/^[0-9]{10}$/, "Phone number must be exactly 10 digits"),
 });
 
 export type OrderDetails = Yup.InferType<typeof schema>;
 
 const initialValues: OrderDetails = {
   name: "",
-  address: "",
-  city: "",
-  zip: "",
   email: "",
-  phone: "",
 };
 
 export function CreateUser() {
@@ -85,74 +73,8 @@ export function CreateUser() {
                     </Form.Control.Feedback>
                   )}
                 </Form.Group>
-                <Form.Group as={Col} md="4" controlId="validationFormik03">
-                  <Form.Label>Adress</Form.Label>
-                  <InputGroup hasValidation>
-                    <Form.Control
-                      data-cy="customer-address"
-                      type="text"
-                      name="address"
-                      value={values.address}
-                      onChange={handleChange}
-                      placeholder="Adress"
-                      isValid={touched.address && !errors.address}
-                      isInvalid={touched.address && !!errors.address}
-                      autoComplete="street-address"
-                    />
-                    {touched.address && errors.address && (
-                      <Form.Control.Feedback
-                        type="invalid"
-                        data-cy="customer-address-error"
-                      >
-                        {errors.address}
-                      </Form.Control.Feedback>
-                    )}
-                  </InputGroup>
-                </Form.Group>
               </Row>
               <Row className="mb-3">
-                <Form.Group as={Col} md="6" controlId="validationFormik04">
-                  <Form.Label>City</Form.Label>
-                  <Form.Control
-                    data-cy="customer-city"
-                    type="text"
-                    placeholder="City"
-                    name="city"
-                    value={values.city}
-                    onChange={handleChange}
-                    isInvalid={touched.city && !!errors.city}
-                    autoComplete="address-level2"
-                  />
-                  {touched.city && errors.city && (
-                    <Form.Control.Feedback
-                      type="invalid"
-                      data-cy="customer-city-error"
-                    >
-                      {errors.city}
-                    </Form.Control.Feedback>
-                  )}
-                </Form.Group>
-                <Form.Group as={Col} md="3" controlId="validationFormik07">
-                  <Form.Label>Zip</Form.Label>
-                  <Form.Control
-                    data-cy="customer-zipcode"
-                    type="text"
-                    placeholder="Zip"
-                    name="zip"
-                    value={values.zip}
-                    onChange={handleChange}
-                    isInvalid={touched.zip && !!errors.zip}
-                    autoComplete="postal-code"
-                  />
-                  {touched.zip && errors.zip && (
-                    <Form.Control.Feedback
-                      type="invalid"
-                      data-cy="customer-zipcode-error"
-                    >
-                      {errors.zip}
-                    </Form.Control.Feedback>
-                  )}
-                </Form.Group>
                 <Form.Group as={Col} md="3" controlId="validationFormik05">
                   <Form.Label>Email</Form.Label>
                   <Form.Control
@@ -171,27 +93,6 @@ export function CreateUser() {
                       data-cy="customer-email-error"
                     >
                       {errors.email}
-                    </Form.Control.Feedback>
-                  )}
-                </Form.Group>
-                <Form.Group as={Col} md="3" controlId="validationFormik06">
-                  <Form.Label>Phone</Form.Label>
-                  <Form.Control
-                    data-cy="customer-phone"
-                    type="text"
-                    placeholder="phone"
-                    name="phone"
-                    value={values.phone}
-                    onChange={handleChange}
-                    isInvalid={touched.phone && !!errors.phone}
-                    autoComplete="tel"
-                  />
-                  {touched.phone && errors.phone && (
-                    <Form.Control.Feedback
-                      type="invalid"
-                      data-cy="customer-phone-error"
-                    >
-                      {errors.phone}
                     </Form.Control.Feedback>
                   )}
                 </Form.Group>
---
+++ client/src/components/TestUser.tsx
@@ -0,0 +1,95 @@
+import { ErrorMessage, Field, Form, Formik } from "formik";
+import * as Yup from "yup";
+
+function TestForm = () => {
+  const initialValues = {
+    name: "",
+    email: "",
+    password: "",
+  };
+
+  const validationSchema = Yup.object({
+    name: Yup.string().required("Name is required"),
+    email: Yup.string().email("Invalid email").required("Email is required"),
+    password: Yup.string().required("Password is required"),
+  });
+
+  const handleSubmit = async (values, { resetForm }) => {
+    try {
+      const response = await fetch("/api/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(values),
+      });
+
+      if (response.ok) {
+        console.log("User created successfully"); // Reset the form after successful submission
+        resetForm();
+      } else {
+        console.error("Error creating user:", response.statusText);
+      }
+    } catch (error) {
+      console.error("Error creating user:", error);
+    }
+  };
+
+  return (
+    <div className="container">
+      <h2>Create User</h2>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={handleSubmit}
+      >
+        <Form>
+          <div className="mb-3">
+            <label htmlFor="name" className="form-label">
+              Name
+            </label>
+            <Field type="text" id="name" name="name" className="form-control" />
+            <ErrorMessage name="name" component="div" className="text-danger" />
+          </div>{" "}
+          <div className="mb-3">
+            <label htmlFor="email" className="form-label">
+              Email
+            </label>
+            <Field
+              type="email"
+              id="email"
+              name="email"
+              className="form-control"
+            />
+            <ErrorMessage
+              name="email"
+              component="div"
+              className="text-danger"
+            />
+          </div>{" "}
+          <div className="mb-3">
+            <label htmlFor="password" className="form-label">
+              Password
+            </label>
+            <Field
+              type="password"
+              id="password"
+              name="password"
+              className="form-control"
+            />
+            <ErrorMessage
+              name="password"
+              component="div"
+              className="text-danger"
+            />
+          </div>{" "}
+          <button type="submit" className="btn btn-primary">
+            Create User
+          </button>
+        </Form>
+      </Formik>
+    </div>
+  );
+};
+
+export default TestForm;
