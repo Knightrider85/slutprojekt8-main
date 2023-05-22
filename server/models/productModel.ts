5ee1555d12f5ffff2a5af45dbf9befec1e8f4c55
Vacannot <simon9916@gmail.com>
2023-05-22 23:48:30 +0200

    produkt model

---
+++ server/models/productModel.ts
@@ -0,0 +1,39 @@
+import mongoose, { Schema, Types } from "mongoose";
+import { Document } from "mongoose";
+
+export interface Product extends Document {
+    id: string,
+    name: string;
+    description: string;
+    price: number;
+    image: string;
+    imageId: Types.ObjectId;
+  
+    stock: number;
+    categories: string[];
+    quantity?: number;
+}
+
+export const ProductSchema = new mongoose.Schema<Product>(
+    {
+      name: { type: String, required: true },
+      description: { type: String, required: true },
+      price: { type: Number, required: true },
+      imageId: { type: Schema.Types.ObjectId, required: true },
+  
+      stock: { type: Number, required: true },
+      categories: { type: [String], required: true },
+    },
+    {
+      toJSON: { virtuals: true },
+      toObject: { virtuals: true },
+    }
+  );
+
+  ProductSchema.virtual("imageUrl").get(function () {
+    return "/api/media/" + this.imageId;
+  });
+  
+  
+  export const ProductModel = mongoose.model("product", ProductSchema);
+  
