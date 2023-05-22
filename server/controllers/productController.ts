1bf347023cf10f3dc976324b2d7a1debce533329
Vacannot <simon9916@gmail.com>
2023-05-23 00:14:49 +0200

    delete product api

--- server/controllers/productController.ts
+++ server/controllers/productController.ts
@@ -1,6 +1,8 @@
 import { NextFunction, Request, Response } from "express";
 import { ProductModel, Product } from "../models/productModel";
 
+// Add a product
+
 export const addProduct = async (
   req: Request<{}, {}, Product>,
   res: Response
@@ -11,5 +13,22 @@ export const addProduct = async (
     res.status(200).json(product);
   } catch (err) {
     console.log(err);
+  }
+};
+
+// Delete a product with ID
+
+export const deleteProduct = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  try {
+    const product = await ProductModel.findByIdAndDelete(id);
+    if (!product) {
+      return res.status(200).json(product);
+    }
+    res.status(200).json(product);
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      res.status(400).json(err.message);
+    }
   }
 };
