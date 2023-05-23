a4c5de53029fe0fdfea5ddf6b8d567b8974c2de1
Martina Kreivi <martinakreivi@gmail.com>
2023-05-23 13:02:51 +0200

    orderRouter

--- server/routes/orderRoutes.ts
+++ server/routes/orderRoutes.ts
@@ -1,3 +1,11 @@
 import express from "express";
 
 const router = express.Router();
+
+export const orderRouter = express
+.Router().get("/order/:id", (req, res) => {
+  // Logic for the product page route
+  const orderId = req.params.id;
+  res.send(`Order Page - ID: ${orderId}`);
+}
+);
