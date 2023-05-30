57e2701a31c093d5063ed4ed0a900a8daa33fd36
Martina Kreivi <martinakreivi@gmail.com>
2023-05-30 14:20:38 +0200

    Co-authored-by: Jenny Weijland <JennyWeij@users.noreply.github.com>

--- server/controllers/fileController.ts
+++ server/controllers/fileController.ts
@@ -1,5 +1,6 @@
 import busboy from 'busboy';
 import { Request, Response } from 'express';
+import { fileBucket } from '/'
 
 export async function getFileById(req: Request, res: Response) {
  //to do implement
@@ -9,11 +10,20 @@ export async function uploadFile(req: Request, res: Response) {
   const bb = busboy({ headers: req.headers });
   req.pipe(bb);
 
-  bb.on('file', (name, file, info) => {
-    const { filename, encoding, mimeType } = info;
-    console.log('NEW FILE', { name, filename, encoding, mimeType });
+  bb.on('file', (_, file, info) => {
+    const { filename, mimeType } = info;
+
+    const uploadStream = fileBucket
+      .openUploadStream(filename, { contentType: mimeType })
+      .on('finish', (data: mongoose.mongo.GridFSFile) => {
+        res.status(201).json(data._id);
+      });
+
+    const resizer = sharp().resize(1080).jpeg({ quality: 90 }).grayscale();
+
+    file.pipe(resizer).pipe(uploadStream);
   });
- }
+}
 
  export async function deleteFileById(req: Request, res: Response) {
   //to do implement
