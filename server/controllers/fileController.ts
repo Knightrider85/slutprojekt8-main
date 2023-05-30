7ec231a510c6e53aed81000d2d74fe02fa51ac3b
Martina Kreivi <martinakreivi@gmail.com>
2023-05-30 12:18:15 +0200

    uploadfile

--- server/controllers/fileController.ts
+++ server/controllers/fileController.ts
@@ -1,3 +1,4 @@
+import busboy from 'busboy';
 import { Request, Response } from 'express';
 
 export async function getFileById(req: Request, res: Response) {
@@ -5,7 +6,13 @@ export async function getFileById(req: Request, res: Response) {
 }
 
 export async function uploadFile(req: Request, res: Response) {
-  //to do implement
+  const bb = busboy({ headers: req.headers });
+  req.pipe(bb);
+
+  bb.on('file', (name, file, info) => {
+    const { filename, encoding, mimeType } = info;
+    console.log('NEW FILE', { name, filename, encoding, mimeType });
+  });
  }
 
  export async function deleteFileById(req: Request, res: Response) {
