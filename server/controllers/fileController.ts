52edaecc912ac572d109e60cc94fe68f828b7a3c
Martina Kreivi <martinakreivi@gmail.com>
2023-05-30 12:09:03 +0200

    added fileRoutes.ts and fileController.ts with setup

---
+++ server/controllers/fileController.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from 'express';
+
+export async function getFileById(req: Request, res: Response) {
+ //to do implement
+}
+
+export async function uploadFile(req: Request, res: Response) {
+  //to do implement
+ }
+
+ export async function deleteFileById(req: Request, res: Response) {
+  //to do implement
+ }
