e9c78ee836efb63ecbf97584db2f9013684c38b6
JennyWeij <jennyweij@outlook.com>
2023-05-31 17:11:40 +0200

    lagt till try catch

--- server/controllers/fileController.ts
+++ server/controllers/fileController.ts
@@ -5,9 +5,10 @@ import sharp from 'sharp';
 import { fileBucket } from '../models/fileModel';
 
 export async function getFileById(req: Request, res: Response) {
+  try {
     if (!req.params.id) return res.json('File not found');
     const _id = new mongoose.mongo.ObjectId(req.params.id);
-    
+
     const file = await fileBucket.find({ _id }).next();
 
     if (!file?.contentType) {
@@ -15,29 +16,40 @@ export async function getFileById(req: Request, res: Response) {
     }
     res.setHeader('Content-Type', file.contentType);
     // res.setHeader('Content-Disposition', `attachment; filename=${file.filename}`);
-  const downloadStream = fileBucket.openDownloadStream(_id);
-  downloadStream.pipe(res);
+    const downloadStream = fileBucket.openDownloadStream(_id);
+    downloadStream.pipe(res);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json('Internal server error');
   }
+}
+
 
 
 export async function uploadFile(req: Request, res: Response) {
-  const bb = busboy({ headers: req.headers });
-  req.pipe(bb);
+  try {
+    const bb = busboy({ headers: req.headers });
+    req.pipe(bb);
 
-  bb.on('file', (_, file, info) => {
-    const { filename, mimeType } = info;
+    bb.on('file', (_, file, info) => {
+      const { filename, mimeType } = info;
 
-    const uploadStream = fileBucket
-      .openUploadStream(filename, { contentType: mimeType })
-      .on('finish', (data: mongoose.mongo.GridFSFile) => {
-        res.status(201).json(data._id);
-      });
+      const uploadStream = fileBucket
+        .openUploadStream(filename, { contentType: mimeType })
+        .on('finish', (data: mongoose.mongo.GridFSFile) => {
+          res.status(201).json(data._id);
+        });
 
-    const resizer = sharp().resize(1080).jpeg({ quality: 90 }).grayscale();
+      const resizer = sharp().resize(1080).jpeg({ quality: 90 }).grayscale();
 
-    file.pipe(resizer).pipe(uploadStream);
-  });
+      file.pipe(resizer).pipe(uploadStream);
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json('Internal server error');
+  }
 }
+
 
  export async function deleteFileById(req: Request, res: Response) {
   //to do implement
